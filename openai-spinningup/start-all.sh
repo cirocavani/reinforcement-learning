a4a789eb79e07e937f033d584e8cf811b47cf141
Ciro Cavani <ciro.cavani@gmail.com>
2019-06-27 18:08:01 -0300

    OpenAI Spinning Up: fix python setup

---
+++ openai-spinningup/start-all.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -eu
+
+cd $HOME
+
+~/bin/start-x.sh
+~/bin/start-jupyter.sh
--- openai-spinningup/start-x.sh
+++ openai-spinningup/start-x.sh
@@ -3,7 +3,7 @@ set -eu
 
 cd $HOME
 
-export DISPLAY=:1
+. activate x
 
 VNC_HOST=localhost
 VNC_PORT=5900
@@ -12,5 +12,3 @@ Xvfb $DISPLAY -screen 0 800x600x24 > ~/logs/xvfb.log 2>&1 &
 fluxbox > ~/logs/fluxbox.log 2>&1 &
 x11vnc -forever -display $DISPLAY -bg -nopw -listen $VNC_HOST -rfbport $VNC_PORT -xkb > ~/logs/x11vnc.log 2>&1 &
 ~/libs/noVNC-1.1.0/utils/launch.sh --vnc $VNC_HOST:$VNC_PORT > ~/logs/novnc.log 2>&1 &
-
-~/bin/start-jupyter.sh
